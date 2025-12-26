6d62910ab392152c416a745de70302a77ba74d21
Zaharov Andrey <zaharov.andrey161@gmail.com>
2025-12-26 14:01:48 +0300

    Added Event and Booking models.
    Made corresponding migrations.

--- back/api/models.py
+++ back/api/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+UserModel = get_user_model()
+
+class Event(models.Model):
+    name = models.CharField(max_length=255)
+    total_seats = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Booking(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="bookings", verbose_name="Мероприятие")
+    user = models.ForeignKey(UserModel, on_delete=models.CASCADE, verbose_name="Пользователь")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["event", "user_id"], name="uniq_booking_per_user_event"),
+        ]
+        indexes = [
+            models.Index(fields=["event"]),
+            models.Index(fields=["event", "created_at"]),
+        ]
+
+    def __str__(self):
+        return f"{self.event}:{self.user.get_full_name()}"
